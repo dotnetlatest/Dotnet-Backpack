90fa2c7f22533dfa42db2fe230d6ff7697cdc869
Samuel Mensah <samuel.nimako-mensah@popagency.com>
2015-05-29 15:20:08 -0700

    updates
    - added enumextenstions
    - added html controls to forms page

--- docs/MVC Helpers Cheat sheet.md
+++ docs/MVC Helpers Cheat sheet.md
@@ -6,4 +6,10 @@
 ```C#
     public 
 ```
->>
+
+http://www.codeproject.com/Articles/787320/An-Absolute-Beginners-Tutorial-on-HTML-Helpers-and
+http://www.slideshare.net/RapPayne/14-html-helpers
+http://www.scribd.com/doc/188104801/ASP-net-MVC-Razor-Helpers-Cheat-Sheet#scribd
+http://httputility.com/various/razorhelperscheatsheet.html
+http://www.elijahmanor.com/asp-net-mvc-cheat-sheets/
+http://www.w3schools.com/html/html_form_input_types.asp
---
+++ src/Backpack.Extensions/EnumExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace Backpack.Extensions
+{
+    public static class EnumExtensions
+    {
+        /// <summary>
+        /// Returns the Description value defined the attribute
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string ToDescriptionString(this Enum value)
+        {
+            Type type = value.GetType();
+
+            MemberInfo[] memInfo = type.GetMember(value.ToString());
+
+            if (memInfo.Length > 0)
+            {
+                object[] description = memInfo[0].GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+                if (description.Length > 0)
+                {
+                    return ((DescriptionAttribute)description[0]).Description;
+                }
+            }
+
+            return value.ToString();
+        } 
+    }
+}
